f20fc7cdcd5491baddafaa8fcea4d8be7928a596
Алексей Скворцов <sotoder@gmail.com>
2021-05-01 01:17:13 +0300

    Выполнено задание 2, улучшен в задании 5

--- SecondLesson/Lesson2/IMT_Homework_L2/Program.cs
+++ SecondLesson/Lesson2/IMT_Homework_L2/Program.cs
@@ -5,7 +5,7 @@ namespace IMT_Homework_L2
     class Program
     {
         // Скворцов А.В.
-        //2.	Ввести вес и рост человека. Рассчитать и вывести индекс массы тела (ИМТ) по формуле I=m/(h*h); где m — масса тела в килограммах, h — рост в метрах.
+        //5. а) Написать программу, которая запрашивает массу и рост человека, вычисляет его индекс массы и сообщает, нужно ли человеку похудеть, набрать вес или всё в норме.
         static void Main(string[] args)
         {
             string name;
@@ -16,97 +16,69 @@ namespace IMT_Homework_L2
 
             Console.Write(name + ", укажите ваш вес в килограммах:");
             string tmpString = Console.ReadLine();
-            weight = SetParam(tmpString);
-
+            weight = SetParam(tmpString, "W");
             Console.Write(name + ", укажите ваш рост в метрах:");
             tmpString = Console.ReadLine();
-            height = SetParam(tmpString);
+            height = SetParam(tmpString, "H");
 
             bmi = CalcBMI(weight, height);
-
-
             Console.WriteLine("Ваш ИМТ равен: {0:f2}", bmi);
 
-            IndexComparsion(bmi, weight, height);
-
+            // б) Рассчитать, на сколько кг похудеть или сколько кг набрать для нормализации веса.
+            IndexComparsion(bmi, weight, height); // Сравниваем ИМТ с нормой и даем советы
             Console.ReadLine();
 
         }
 
-        private static void IndexComparsion(double bmi, double weight, double height)
+        private static void IndexComparsion(double bmi, double weight, double height) 
         {
-            if (bmi < 18.5)
+            string comparsionResult = bmi switch
             {
-                Console.WriteLine("Вы еще тут? Ветром не сдуло? Вас бы накормить. Вам надо нажраТ минимум {0:f2} кг", CalcFat(weight, height, false));
-            }
-            else if (bmi >= 18.5 && bmi <= 24.9)
-            {
-                Console.WriteLine("Ваш ИМТ в норме! Лови пятюню");
-            }
-            else if (bmi > 24.9 && bmi <= 29.9)
-            {
-                Console.WriteLine("Вы чет пухленький, может физкультурки? Вам надо скинуть минимум {0:f2} кг", CalcFat(weight, height, true));
-            }
-            else if (bmi > 29.9 && bmi <= 35.4)
-            {
-                Console.WriteLine("Вы сильно пухленький, может диеты с физкультуркой? Вам надо скинуть минимум {0:f2} кг", CalcFat(weight, height, true));
-            }
-            else
-            {
-                Console.WriteLine("Да вы прям жирненький, попробуйте не жраТ! Вам надо скинуть минимум {0:f2} кг", CalcFat(weight, height, true));
-            }
+                < 18.5 => $"Вы еще тут? Ветром не сдуло? Вас бы накормить. Вам надо нажраТ минимум {CalcFat(weight, height, false)} кг",
+                >= 18.5 and <= 24.9 => $"Ваш ИМТ в норме! Лови пятюню",
+                > 24.9 and <= 29.9 => $"Вы чет пухленький, может физкультурки? Вам надо скинуть минимум {CalcFat(weight, height, true)} кг",
+                > 29.9 and <= 35.4 => $"Вы сильно пухленький, может диеты с физкультуркой? Вам надо скинуть минимум {CalcFat(weight, height, true)} кг",
+                _ => $"Да вы прям жирненький, попробуйте не жраТ! Вам надо скинуть минимум {CalcFat(weight, height, true)} кг"
+            };
+             Console.WriteLine(comparsionResult);
         }
 
-        private static float CalcBMI(double weight, double height)
+        private static double CalcBMI(double weight, double height) // Расчет ИМТ
         {
-            return (float)Math.Round(weight / (height * height), 2);
+            return Math.Round(weight / (height * height), 2);
         }
 
-        private static object CalcFat(Double weight, Double height, Boolean fat)
+        private static object CalcFat(double weight, double height, bool fat) //Расчет избытка или недостатка веса относительно нормы
         {
-            if (fat)
-            {
-                double normalMaxWight = 24.9 * Math.Pow(height, 2);
-                return weight - normalMaxWight;
-            } else
-            {
-                double normalMinWight = 18.5 * Math.Pow(height, 2);
-                return Math.Abs(weight - normalMinWight);
-            }
 
+            double normalMaxWight = 24.9 * Math.Pow(height, 2);
+            double normalMinWight = 18.5 * Math.Pow(height, 2);
+            return Math.Abs(Math.Round(weight - (fat ? normalMaxWight : normalMinWight), 2));
         }
 
-        private static double SetParam(string strFromConsole)
+        private static double SetParam(string strFromConsole, string paramName)
         {
             double param = 0;
 
             while (param == 0)
             {
-                if (NumbersEntered(strFromConsole))
+                if (double.TryParse(strFromConsole, out double checkedDouble))
                 {
-                    param = double.Parse(strFromConsole);
+                    param = checkedDouble;
                 }
                 else
                 {
-                    Console.Write("Пожалуйста, вводите значения веса/роста с помощью цифр. Повторите ввод:");
+                    string checkParamName = paramName switch
+                    {
+                        "W" => "Пожалуйста, вводите значения веса цифрами. Повторите ввод:",
+                        "H" => "Пожалуйста, вводите значения роста цифрами. Повторите ввод:",
+                        _=>""
+                    };
+                    Console.WriteLine(checkParamName);
                     strFromConsole = Console.ReadLine();
                 }
             }
             return param;
-        }
-
-        private static bool NumbersEntered(string checkedStr)
-        {
-            double checkedDouble;
-
-            if (double.TryParse(checkedStr, out checkedDouble))
-            {
-                return true;
-            }
-            else
-            {
-                return false;
-            };
         }
     }
 }
---
+++ SecondLesson/Lesson2/MinNumber/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace MinNumber
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите число(желательно побольше)");
+            string num = SetParam(Console.ReadLine());
+            int sum = CountNumbersInNumber(num);
+            Console.WriteLine($"Число состоит из {sum} чисел");
+            Console.ReadLine();
+        }
+
+        private static int CountNumbersInNumber(string num)
+        {
+            int sum = 0;
+            foreach (char a in num)
+            {
+                sum++;
+            }
+            return sum;
+        }
+
+        private static string SetParam(string strFromConsole)
+        {
+            int param = 0;
+
+            while (param == 0)
+            {
+                if (Int32.TryParse(strFromConsole, out int checkedInt))
+                {
+                    param = checkedInt;
+                }
+                else
+                {
+                    Console.Write("Пожалуйста, вводите только целые числа (десятичные будут округлены до целого). Повторите ввод:");
+                    strFromConsole = Console.ReadLine();
+                }
+            }
+            return param.ToString();
+        }
+    }
+}
