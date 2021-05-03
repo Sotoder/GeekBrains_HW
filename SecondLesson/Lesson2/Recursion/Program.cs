9ffd62d243307b24aaf19e43ddb6a670c029bcff
Алексей Скворцов <sotoder@gmail.com>
2021-05-03 11:29:06 +0300

    Переписал метод рекурсии, объединив две повторяющие друг друга функции в один метод, который вносит итоговые значения в глобальные переменные. Кроме того в старой реализации не обнулял глобальные переменные класса.

--- SecondLesson/Lesson2/Recursion/Program.cs
+++ SecondLesson/Lesson2/Recursion/Program.cs
@@ -6,8 +6,8 @@ namespace Recursion
     //Скворцов А.В.
     class Program
     {
-        public string resultStr="";
-        public int resultSum = 0;
+        public string resultStr;
+        public int resultSum;
         static void Main(string[] args)
         {
             Program program = new();
@@ -39,30 +39,24 @@ namespace Recursion
                 }
             }
 
-            Console.WriteLine($"Последовательность чисел от {a} до {b}: {WriteSequence(a, b)}");
-            Console.WriteLine($"Сумма всех чисел от {a} до {b}: {WriteSum(a, b)}");
+            CalcSumCollectSeq(a, b);
+            Console.WriteLine($"Последовательность чисел от {a} до {b}: {resultStr}");
+            Console.WriteLine($"Сумма всех чисел от {a} до {b}: {resultSum}");
         }
 
-        //б) * Разработать рекурсивный метод, который считает сумму чисел от a до b.
-        private object WriteSum(int a, int b)
+        private void CalcSumCollectSeq(int a, int b)
         {
+            // на всякий случай обнуляем глобальные переменные
+            resultStr = "";
+            resultSum = 0;
+
+            // запускаем рекурсию
             if (a <= b)
             {
-                WriteSum(a, b - 1);
+                CalcSumCollectSeq(a, b - 1);
                 resultSum += b;
-            }
-            return resultSum;
-        }
-
-        ////a) Разработать рекурсивный метод, который выводит на экран числа от a до b(a<b).
-        private string WriteSequence(int a, int b)
-        {
-            if (a <= b)
-            {
-                WriteSequence(a, b-1);
                 resultStr += (b == a ? "" : ", ") + b.ToString();
             }
-            return resultStr;
         }
     }
 }
