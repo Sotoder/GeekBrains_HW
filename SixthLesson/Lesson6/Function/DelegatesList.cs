f163a4789d4e95eca874b08df2156faf143fe493
Алексей Скворцов <sotoder@gmail.com>
2021-05-15 22:20:46 +0300

    Сделано 1 задание 6го урока.

---
+++ SixthLesson/Lesson6/Function/DelegatesList.cs
@@ -0,0 +1,10 @@
+﻿namespace Function
+{
+    public class DelegatesList
+    {
+        //Решил проверить, могу ли я сослаться на делегатов другого класса - Могу)
+        //Да и чего бы не вынести список делегатов в отдельный лист.
+        public delegate double Fun(double x);
+        public delegate double Fun2(double x, double y);
+    }
+}
---
+++ SixthLesson/Lesson6/Function/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+// Описываем делегат. В делегате описывается сигнатура методов, на
+// которые он сможет ссылаться в дальнейшем (хранить в себе)
+
+namespace Function
+{
+    
+    class Program
+    {
+        static void Main()
+        {
+            Paint test = new Paint();
+            test.PaintStart();
+        }
+    }
+}
+
+
