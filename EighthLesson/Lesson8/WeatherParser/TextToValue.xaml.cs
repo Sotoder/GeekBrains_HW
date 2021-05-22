61b87a564b1c05d2d23b5fe1e747b29c917f533d
Алексей Скворцов <sotoder@gmail.com>
2021-05-22 18:25:44 +0300

    Второе задание сделал, после скорее всего будет заюзано для выбора даты

---
+++ EighthLesson/Lesson8/WeatherParser/TextToValue.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace WeatherParser
+{
+    /// <summary>
+    /// Логика взаимодействия для TextToValue.xaml
+    /// </summary>
+    public partial class TextToValue : Window
+    {
+        public TextToValue()
+        {
+            InitializeComponent();
+            calendar.SelectedDate = DateTime.Now;
+        }
+
+        private void calendar_SelectedDatesChanged(object sender, SelectionChangedEventArgs e)
+        {
+            tbDate.Text = calendar.SelectedDate.Value.ToString("dd-MM-yy");
+        }
+    }
+}
